3aeb5af29b5d0bf689ccf0193e3c2a88d14f8e11
Ichlasul Bulqiah <bichlasulbulqiah@gmail.com>
2024-01-14 13:39:46 +0700

    rework test level layout

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,21 +1,33 @@
-extends CharacterBody2D
+class_name Player extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity_direction = Vector2(0, 1)
+
+
+enum PlayerState {
+	IDLE,
+	WALK,
+	JUMP,
+	ATTACK,
+}
+
+
+func _jump_state():
+	pass
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity += (gravity * delta) * gravity_direction
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity = JUMP_VELOCITY * gravity_direction
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,6 +35,14 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED/2)
 
 	move_and_slide()
+
+
+func _input(event):
+	if event.is_action_pressed("ui_interact"):
+		gravity_direction *= -1
+		up_direction = gravity_direction * -1
+		
+		rotation_degrees = rad_to_deg(Vector2.ZERO.angle_to_point(gravity_direction)) - 90
