fa738f02ac1fcf8803130350dba066007a497250
Ichlasul Bulqiah <bichlasulbulqiah@gmail.com>
2024-01-14 17:41:47 +0700

    add gravity rotate and camera tweening

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,11 +1,13 @@
 class_name Player extends CharacterBody2D
 
+signal gravity_direction_changed(gravity_direction)
+
 
 const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var gravity_direction = Vector2(0, 1)
+@export var gravity_direction = Vector2(0, 1)
 
 
 enum PlayerState {
@@ -14,6 +16,17 @@ enum PlayerState {
 	JUMP,
 	ATTACK,
 }
+
+
+func rotate_gravity(angle_degree:float):
+	var current_gravity_angle =  Vector2.ZERO.angle_to_point(gravity_direction)
+	current_gravity_angle += deg_to_rad(angle_degree)
+	gravity_direction = Vector2(round(cos(current_gravity_angle)), round(sin(current_gravity_angle)))
+	up_direction = gravity_direction * -1
+	
+	emit_signal("gravity_direction_changed", gravity_direction)
+	
+	return current_gravity_angle
 
 
 func _jump_state():
@@ -31,18 +44,27 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED/2)
+	#print(round(gravity_direction.x), " ", round(gravity_direction.y))
+	if not gravity_direction.x:
+		var direction = Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	elif not gravity_direction.y:
+		var direction = Input.get_axis("ui_up", "ui_down")
+		if direction:
+			velocity.y = direction * SPEED
+		else:
+			velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
 
 
 func _input(event):
-	if event.is_action_pressed("ui_interact"):
-		gravity_direction *= -1
-		up_direction = gravity_direction * -1
-		
-		rotation_degrees = rad_to_deg(Vector2.ZERO.angle_to_point(gravity_direction)) - 90
+	if event.is_action_pressed("ui_rotate_gravity_90"):
+		rotation_degrees = rad_to_deg(rotate_gravity(-90)) - 90
+	
+	elif event.is_action_pressed("ui_rotate_gravity_-90"):
+		rotation_degrees = rad_to_deg(rotate_gravity(90)) - 90
---
+++ scripts/PlayerCamera.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+
+
+const Y_OFFSET = 80
+const X_OFFSET = 80
+
+
+func _on_player_gravity_direction_changed(gravity_direction):
+	var tween = create_tween()
+	if gravity_direction == Vector2.DOWN or gravity_direction == Vector2.UP:
+		tween.tween_property(self, "offset", Vector2(0, Y_OFFSET * -gravity_direction.y), 0.5)
+	
+	if gravity_direction == Vector2.LEFT or gravity_direction == Vector2.RIGHT:
+		tween.tween_property(self, "offset", Vector2(X_OFFSET * -gravity_direction.x, 0), 0.5)
