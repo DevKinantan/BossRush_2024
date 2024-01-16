97b6954fc78da5c5a5b098fed00072ce653ec712
Ichlasul Bulqiah <bichlasulbulqiah@gmail.com>
2024-01-16 18:45:44 +0700

    change camera work

--- scripts/PlayerCamera.gd
+++ scripts/PlayerCamera.gd
@@ -1,15 +1,15 @@
 extends Camera2D
 
 
-const Y_OFFSET = 100
-const X_OFFSET = 100
+@export var offset_power:float = 0.2
+
+var player: Player
 
 
-func _on_player_gravity_direction_changed(gravity_direction):
-	#return
-	var tween = create_tween()
-	if gravity_direction == Vector2.DOWN or gravity_direction == Vector2.UP:
-		tween.tween_property(self, "offset", Vector2(0, Y_OFFSET * -gravity_direction.y), 0.5)
-	
-	if gravity_direction == Vector2.LEFT or gravity_direction == Vector2.RIGHT:
-		tween.tween_property(self, "offset", Vector2(X_OFFSET * -gravity_direction.x, 0), 0.5)
+func _ready():
+	player = get_parent()
+
+
+func _process(_delta):
+	var mouse_pos_from_player = get_global_mouse_position() - player.global_position
+	offset = mouse_pos_from_player * 0.2
